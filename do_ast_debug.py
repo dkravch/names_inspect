79168f277db3464a899de2ecded1c9b9a7c9bd79
Denys Kravchenko <dkravchenko@truveris.com>
2023-07-12 15:55:05 +0300

    Added GeneralNode class

--- do_ast_debug.py
+++ do_ast_debug.py
@@ -2,8 +2,50 @@ import ast
 
 
 SAMPLE = "a=1;\nb=2;"
+SAMPLE = """
+
+class Banana:
+
+    def banana(self, x):
+        inner_var = 3 
+        self.y = x
+
+"""
 
 ast_root = ast.parse(SAMPLE)
-
 print(ast.dump(ast_root, indent=2))
+
+for node in ast.walk(ast_root):
+    print(node)
+    try:
+        # print(dir(node))
+        print(node.value.id, "<<<<<<<<<")
+        print(node.id)
+
+    except:
+        try:
+            print(node.name)
+        except:
+            ...
+
+
+exit(0)
+
+
 print(ast_root.body[0].col_offset, ast_root.body[0].lineno)
+
+
+exit()
+
+
+class MyVisitor(ast.NodeVisitor):
+    def visit_ClassDef(self, node):
+        body = node.body
+        for statement in node.body:
+            if isinstance(statement, ast.Assign):
+                if len(statement.targets) == 1 and isinstance(statement.targets[0], ast.Name):
+                    print('class: %s, %s=%s' % (str(node.name), str(statement.targets[0].id), str(statement.value)))
+
+
+tree = ast.parse(open('/home/dkravchenko/WORK/sources/Bitbucket/ttf_udp/base/udp_env.py').read(), '')
+MyVisitor().visit(tree)
--- do_validate_names.py
+++ do_validate_names.py
@@ -7,10 +7,71 @@ import numpy as np
 from sklearn.cluster import AffinityPropagation
 import distance
 
-PATH = os.environ.get("BANANA", "/home/dkravchenko/WORK/sources/Bitbucket/ttf_udp")
+PATH = os.environ.get("BANANA", "/home/dkravchenko/WORK/ON_THE_ROAD/CODE/NOT_MY/ttf_udp")
 EXCLUDE_NAMES = keyword.kwlist + [i for i in dir(builtins) if not i.startswith("__")]
 
+# TODO Variable like self.smth, cls.smth, in-class defi
+
 ########################################################################################################################
+
+
+class GeneralNode:
+
+    def __init__(self, node):
+
+        self.general_name = getattr(node, 'id', None) or getattr(node, 'name', None) or getattr(node, 'attr', None)
+
+        if self.general_name:
+            self.name_type = ('id',
+                              'name',
+                              'attr')[[idx for idx, val in enumerate([hasattr(node, 'id'),
+                                                                      hasattr(node, 'name'),
+                                                                      hasattr(node, 'attr')]) if val is True][0]]
+        else:
+            self.name_type = None
+
+        self.instance = node
+
+    def is_considered(self):
+
+        if not self.general_name or not self.general_name:
+            return False
+
+        if not isinstance(self.instance, (ast.Name, ast.Attribute)):
+            return False
+
+        if isinstance(getattr(self.instance, 'ctx', None), ast.Load):
+            return False
+
+        if self.name_type in ('id', 'name'):
+            if self.general_name in EXCLUDE_NAMES:
+                return False
+
+        return True
+
+
+########################################################################################################################
+
+
+def _get_names(root):
+    res = []
+    for node in ast.walk(root):
+        general_node = GeneralNode(node)
+        if general_node.is_considered():
+            res.append(general_node.general_name)
+    return sorted(set(res))
+
+
+def _get_name_nodes(root):
+    res = []
+    for node in ast.walk(root):
+        general_node = GeneralNode(node)
+        if general_node.is_considered():
+            res.append(general_node.instance)
+    return res
+
+########################################################################################################################
+
 
 for root, dirs, files in os.walk(PATH, topdown=False):
     for name in files:
@@ -29,13 +90,8 @@ for root, dirs, files in os.walk(PATH, topdown=False):
             classes = [node.name for node in ast.walk(ast_root) if isinstance(node, ast.ClassDef)]
             import_nodes = [node for node in ast.walk(ast_root) if isinstance(node, (ast.Import, ast.ImportFrom))]
 
-            names = sorted({node.id for node in ast.walk(ast_root)
-                            if isinstance(node, ast.Name) and node.id not in EXCLUDE_NAMES
-                            and not isinstance(node.ctx, ast.Load)})  # to only handle names assigned to, not used
-
-            name_nodes = [node for node in ast.walk(ast_root)
-                          if isinstance(node, ast.Name) and node.id not in EXCLUDE_NAMES
-                          and not isinstance(node.ctx, ast.Load)]  # to only handle names assigned to, not used
+            names = _get_names(ast_root)
+            name_nodes = _get_name_nodes(ast_root)
 
             max_name_length = max([len(i) for i in names] if names else [0])
             max_func_length = max([len(f.name) for f in function_nodes] if function_nodes else [0])
@@ -44,7 +100,8 @@ for root, dirs, files in os.walk(PATH, topdown=False):
             grouped_names = []
             if names:
                 words = np.asarray(names)
-                lev_similarity = -1 * np.array([[distance.levenshtein(w1, w2) for w1 in words] for w2 in words], dtype=float)
+                lev_similarity = -1 * np.array([[distance.levenshtein(w1, w2) for w1 in words] for w2 in words],
+                                               dtype=float)
                 affprop = AffinityPropagation(affinity="precomputed", damping=0.5)
                 affprop.fit([list(item) for item in lev_similarity])
 
@@ -57,14 +114,17 @@ for root, dirs, files in os.walk(PATH, topdown=False):
 
             print("\n(names, assigned to only, not used)")
             for cluster in grouped_names:
-                # print(f"<<<< {cluster}")
-                filtered_name_nodes = sorted([node for node in name_nodes if node.id in cluster],
-                                             key=lambda x: x.id)
-                for node in filtered_name_nodes:
-                    if node.id not in cluster:
+                filtered_name_nodes = sorted([GeneralNode(node) for node in name_nodes
+                                              if getattr(node, 'id', None) or getattr(node, 'attr', None) in cluster],
+                                             key=lambda x: getattr(x.instance, 'id', None) or getattr(x.instance,
+                                                                                                      'attr', None))
+
+                for general_node in filtered_name_nodes:
+                    if not general_node.general_name:
                         continue
-                    print(str(node.id).ljust(max_name_length, " "),
-                          str(node.lineno).ljust(6, " "), content_lines[node.lineno - 1])
+                    print(str(general_node.general_name).ljust(max_name_length, " "),
+                          str(general_node.instance.lineno).ljust(6, " "),
+                          content_lines[general_node.instance.lineno - 1])
 
             print("\n(functions)")
             for function in function_nodes:
